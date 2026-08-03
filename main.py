19ce43549707bfeb919584f8b5d6ae56d2b089f0
TheStephanieHuno <thestephaniee@gmail.com>
2026-08-03 18:52:45 +0000

    added configurations for the frontend

--- main.py
+++ main.py
@@ -86,12 +86,15 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://finance-tracker-frontend-ebip.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 # health check endpoint 
 @app.get("/health")
